310d62dddeb420abe2582aa525f062e7afe882bd
Joe Kutner <jpkutner@gmail.com>
2017-04-20 11:26:31 -0500

    Extracted Java specific CLI

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 'use strict';
 exports.topic = {
   name: 'java',
-  description: 'Client tools for Heroku Exec'
+  description: 'Client tools for Java on Heroku'
 };
 
 exports.commands = [
   require('./commands/jconsole.js')('java', 'jconsole'),
   require('./commands/jvisualvm.js')('java', 'visualvm'),
   require('./commands/jstack.js')('java', 'jstack'),
-  require('./commands/jmap.js')('java', 'jmap'),
-
-  require('./commands/ssh.js')('ps', 'exec'),
-  require('./commands/socks.js')('ps', 'socks'),
-  require('./commands/port.js')('ps', 'forward'),
-  require('./commands/copy.js')('ps', 'copy'),
-
-  require('./commands/ssh.js')('dyno', 'exec'),
-  require('./commands/socks.js')('dyno', 'socks'),
-  require('./commands/port.js')('dyno', 'forward'),
-  require('./commands/copy.js')('dyno', 'copy')
+  require('./commands/jmap.js')('java', 'jmap')
 ];
